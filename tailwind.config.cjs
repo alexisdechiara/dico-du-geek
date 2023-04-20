5bedf13b797ad38272bc9191a162176ec20c770c
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2023-04-20 14:42:30 +0200

    🎨Mise en place de la page Histoire #860qjprgq
    🆕 ajout(ContentHeader.astro) : créer un composant ContentHeader pour afficher le titre, la prononciation et les tags d'un contenu
    
    🆕 ajout(histoire.astro) : ajouter une section pour afficher l'histoire d'un terme geek
    
    🆕 ajout(package.json) : ajouter la dépendance @tailwindcss/typography pour utiliser la typographie de Tailwind
    
    🐛 corriger(server.ts) : changer la casse de la variable de port de minuscules à majuscules (PORT)
    
    ✨ fonctionnalité(tailwind.config.cjs) : ajouter la configuration de la typographie pour les styles de prose

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -143,6 +143,83 @@ module.exports = {
 					fontWeight: '600'
 				}]
 			},
+			typography: ({ theme }) => ({
+				DEFAULT: {
+					css: {
+						'--tw-prose-body': theme('colors.light[primary]'),
+						'--tw-prose-headings': theme('colors.light[ternary]'),
+						'--tw-prose-lead': theme('colors.light[ternary]'),
+						'--tw-prose-links': theme('colors.light[ternary]'),
+						'--tw-prose-bold': theme('colors.light[secondary]'),
+						'--tw-prose-counters': theme('colors.light[secondary]'),
+						'--tw-prose-bullets': theme('colors.light[secondary]'),
+						'--tw-prose-hr': theme('colors.light[border]'),
+						'--tw-prose-quotes': theme('colors.light[primary]'),
+						'--tw-prose-quote-borders': theme('colors.light[ternary]'),
+						'--tw-prose-captions': theme('colors.light[ternary]'),
+						'--tw-prose-code': theme('colors.light[primary]'),
+						'--tw-prose-pre-code': theme('colors.light[background]'),
+						'--tw-prose-pre-bg': theme('colors.light[primary]'),
+						'--tw-prose-th-borders': theme('colors.light[border]'),
+						'--tw-prose-td-borders': theme('colors.light[background]'),
+
+						'--tw-prose-invert-body':theme('colors.dark[primary]'),
+						'--tw-prose-invert-headings': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-lead': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-links': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-bold': theme('colors.dark[secondary]'),
+						'--tw-prose-invert-counters': theme('colors.dark[secondary]'),
+						'--tw-prose-invert-bullets': theme('colors.dark[secondary]'),
+						'--tw-prose-invert-hr': theme('colors.dark[border]'),
+						'--tw-prose-invert-quotes': theme('colors.dark[primary]'),
+						'--tw-prose-invert-quote-borders': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-captions': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-code': theme('colors.dark[primary]'),
+						'--tw-prose-invert-pre-code': theme('colors.dark[background]'),
+						'--tw-prose-invert-pre-bg': theme('colors.dark[primary]'),
+						'--tw-prose-invert-th-borders': theme('colors.dark[border]'),
+						'--tw-prose-invert-td-borders': theme('colors.dark[background]'),
+						
+					},
+				},
+				lore: {
+					css: {
+						'--tw-prose-body': theme('colors.light.lore[primary]'),
+						'--tw-prose-headings': theme('colors.light.lore[ternary]'),
+						'--tw-prose-lead': theme('colors.light.lore[ternary]'),
+						'--tw-prose-links': theme('colors.light.lore[ternary]'),
+						'--tw-prose-bold': theme('colors.light.lore[secondary]'),
+						'--tw-prose-counters': theme('colors.light.lore[secondary]'),
+						'--tw-prose-bullets': theme('colors.light.lore[secondary]'),
+						'--tw-prose-hr': theme('colors.light.lore[border]'),
+						'--tw-prose-quotes': theme('colors.light.lore[primary]'),
+						'--tw-prose-quote-borders': theme('colors.light.lore[ternary]'),
+						'--tw-prose-captions': theme('colors.light.lore[ternary]'),
+						'--tw-prose-code': theme('colors.light.lore[primary]'),
+						'--tw-prose-pre-code': theme('colors.light.lore[background]'),
+						'--tw-prose-pre-bg': theme('colors.light.lore[primary]'),
+						'--tw-prose-th-borders': theme('colors.light.lore[border]'),
+						'--tw-prose-td-borders': theme('colors.light.lore[background]'),
+
+						'--tw-prose-invert-body':theme('colors.dark[primary]'),
+						'--tw-prose-invert-headings': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-lead': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-links': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-bold': theme('colors.dark[secondary]'),
+						'--tw-prose-invert-counters': theme('colors.dark[secondary]'),
+						'--tw-prose-invert-bullets': theme('colors.dark[secondary]'),
+						'--tw-prose-invert-hr': theme('colors.dark[border]'),
+						'--tw-prose-invert-quotes': theme('colors.dark[primary]'),
+						'--tw-prose-invert-quote-borders': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-captions': theme('colors.dark[ternary]'),
+						'--tw-prose-invert-code': theme('colors.dark[primary]'),
+						'--tw-prose-invert-pre-code': theme('colors.dark[background]'),
+						'--tw-prose-invert-pre-bg': theme('colors.dark[primary]'),
+						'--tw-prose-invert-th-borders': theme('colors.dark[border]'),
+						'--tw-prose-invert-td-borders': theme('colors.dark[background]'),
+					}
+				},
+			}),
 		},
 	},
 	safelist: [
@@ -151,5 +228,7 @@ module.exports = {
 			variants: ['sm','md','lg','xl','hover','dark']
 		}
 	],
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
